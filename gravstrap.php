3b5f5acd6ab4dd637502d50b51d56c6802827615
Giansimon <giansimon.diblas@gmail.com>
2015-12-09 12:55:48 +0100

    refactored gravstrap.php removed base_component.html.twig because module page must be retrieved in each template

--- gravstrap.php
+++ gravstrap.php
@@ -19,6 +19,7 @@ namespace Grav\Plugin;
 
 use \Grav\Common\Plugin;
 use \Grav\Common\Page\Page;
+use \Grav\Common\Page\Collection;
 
 class GravstrapPlugin extends Plugin
 {
@@ -52,30 +53,28 @@ class GravstrapPlugin extends Plugin
     }
 
     /**
-     * Set needed variables to display breadcrumbs.
+     * Configures Gravstrap
      */
     public function onTwigSiteVariables()
     {
         $config = $this->grav["config"]->get('plugins.gravstrap');
-        
+
         $twig = $this->grav['twig'];
         $gravstrapComponents = array();
         $header = $this->grav["page"]->header();
         if (property_exists($header, "gravstrap")) {
-            $gravstrapComponents = $header->gravstrap;
-            $collection = $this->grav['page']->collection();
-            $gravstrapComponents = array_merge($gravstrapComponents, $this->parseChildren($collection));
+            $gravstrapComponents = array_merge($header->gravstrap, $this->findGravstrapConfigInModules());
         }
-        
+
         if (array_key_exists("gravstrap", $twig->twig_vars['site'])) {
             $gravstrapComponents = array_merge_recursive($twig->twig_vars['site']['gravstrap'], $gravstrapComponents);
         }
-        
+
         $sections = $twig->twig_vars['sections'];
         $gravstrap = array();
         $gravstrapCollection = array();
         foreach($gravstrapComponents as $type => $components) {
-            $components = $this->configureElement($type, $config, $components);            
+            $components = $this->configureElement($type, $config, $components);
             $template = sprintf('%s.html.twig', $type);
             foreach($components as $name => $element) {
                 if (array_key_exists('from_file', $element)) {
@@ -84,38 +83,50 @@ class GravstrapPlugin extends Plugin
                 $gravstrapCollection[$type][] = $gravstrap[$name] = $twig->twig->render($template, array($type => $element));
             }
         }
-        
+
         $twig->twig_vars['gravstrap'] = $gravstrap;
         $twig->twig_vars['gravstrap_collection'] = $gravstrapCollection;
     }
     
-    protected function parseChildren($children)
+    /**
+     * Parses page modules to look up for Gravstrap configuration
+     * 
+     * @return array
+     */
+    protected function findGravstrapConfigInModules()
     {
-        if (is_array($children)) {
+        $collection = $this->grav['page']->collection();
+        if (is_array($collection)) {
             return array();
         }
         
+        return $this->parseChildren($collection);
+    }
+
+    
+    private function parseChildren(Collection $children)
+    {
         $modules = $children->modular();
         if (count($modules) == 0) {
             return array();
         }
-        
+
         $configurations = array();
         foreach($children as $child) {
             $header = $child->header();
             if (property_exists($header, "gravstrap")) {
                 $gravstrapComponents = $this->addModulePageToGravstrap($header->gravstrap, $child);
-                
+
                 $configurations = array_merge($configurations, $gravstrapComponents);
             }
         }
-        
+
         return $configurations;
     }
-    
+
     private function addModulePageToGravstrap($configuration, Page $page)
     {
-        $gravstrapComponents = array(); 
+        $gravstrapComponents = array();
         foreach($configuration as $name => $values) {
             foreach($values as $name1 => $value1) {
                 $gravstrapComponents[$name][$name1] = array_merge(array(
@@ -123,27 +134,27 @@ class GravstrapPlugin extends Plugin
                 ), $value1);
             }
         }
-        
+
         return $gravstrapComponents;
     }
-    
+
     private function configureElement($type, $config, $components)
     {
         if ( ! array_key_exists($type, $config)) {
             return $components;
         }
-        
+
         $className = ucfirst($type);
         $classFile = __DIR__ . sprintf('/classes/%s.php', $className);
         if (!file_exists($classFile)) {
             return $components;
         }
-        
+
         require_once $classFile;
         $className = 'Grav\\Plugin\\' . $className;
         $element = new $className($this->grav);
         $element->process($config[$type], $components);
-        
+
         return $element->processedComponents();
     }
 }
